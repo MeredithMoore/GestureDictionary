356dee29a88fe8f2067780d46af240759758487a
Eloise Dietz <eloise.r.dietz.17@dartmouth.edu>
2016-01-24 03:05:52 -0700

    Add TranslateView

---
+++ Beginners-Luck-C/TranslateViewController.h
@@ -0,0 +1,14 @@
+//
+//  TranslateController.h
+//  Beginners-Luck-C
+//
+//  Created by Eloise Dietz on 1/24/16.
+//  Copyright © 2016 Sai Reddy. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+@interface TranslateViewController : UIViewController
+
+
+@end
